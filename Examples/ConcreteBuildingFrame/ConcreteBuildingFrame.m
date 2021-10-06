1a3ce9d943aeeffd766227653cc2cfd4dddb3d8b
yotsuki3 <43482322+yotsuki3@users.noreply.github.com>
2021-10-06 09:51:55 -0400

    Update ConcreteBuildingFrame.m
    Change to use TRR and eigenvector difference approach by default.

--- Examples/ConcreteBuildingFrame/ConcreteBuildingFrame.m
+++ Examples/ConcreteBuildingFrame/ConcreteBuildingFrame.m
@@ -22,7 +22,7 @@ structModel.K_j = K_j;
 optimzOpts.tolFun = 1e-10;
 optimzOpts.tolX = 1e-10;
 optimzOpts.toolBox  = 'lsqnonlin';
-optimzOpts.optAlgorithm = 'Levenberg-Marquardt';
+optimzOpts.optAlgorithm = 'trust-region-reflective';
 optimzOpts.gradSel = 'on';
 optimzOpts.maxIter = 1e3;
 optimzOpts.maxFunEvals = 12e4;
@@ -54,7 +54,7 @@ expModes.resWeights = ones(n_modes,1);
 
 
 %% Model updating parameter
-updatingOpts.formID = 3.0;       % 1: Modal property diff (MAC) ;
+updatingOpts.formID = 2.0;       % 1: Modal property diff (MAC) ;
                                  % 2: Modal property diff (V_mDiff);
                                  % 3: Modal dynamic residual;
 updatingOpts.modeMatch = 2;      % 1: Without forced matching;
